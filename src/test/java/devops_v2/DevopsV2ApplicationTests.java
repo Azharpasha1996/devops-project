115773481aa73be95531c27dc1fd4e715517bacb
azhar <azharpasha791@gmail.com>
2025-03-04 13:10:53 +0530

    devops

---
+++ src/test/java/devops_v2/DevopsV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package devops_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
